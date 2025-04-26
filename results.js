d53bd717e25a5b96e6ec06f7cf980689ec2eae51
KirstenMc88 <40646660@live.napier.ac.uk>
2025-04-26 11:10:14 +0100

    defined variables for scoring

--- results.js
+++ results.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
 	let endPage = getQueryParam("end"); // || "Pro_Football_Hall_of_Fame";
 	let clicks = getQueryParam("clicks"); //|| 3;
 	let time = getQueryParam("time"); // || 273;
+
+// variables to assist in scoring
+	let difficulty = 1; // default, will be updated after fetching levels.json
+	let optimalClicks = 3; // default, will be updated after fetching levels.json
+
 	
 	//testing console log to check arameters are being picked up
 	console.log("Extracted Parameters:", { startPage, endPage, clicks, time }); 
